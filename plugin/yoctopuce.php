8b80b93c0fcdfaca5cbede0d7469809c6098112b
Patrick Huber <stackmagic@gmail.com>
2014-02-19 13:11:35 +0100

    make yoctopuce plugin work, needs extra yoctopuce_collectd_types.db file (see https://github.com/stackmagic/collectd-yoctopuce) (note: works with tag v0.4.1 -- master is brooken)"

--- plugin/yoctopuce.php
+++ plugin/yoctopuce.php
@@ -1,6 +1,6 @@
 <?php
 
-# Collectd Yoctopuce plugin
+# Collectd yoctopuce plugin
 
 require_once 'conf/common.inc.php';
 require_once 'type/Default.class.php';
@@ -10,28 +10,34 @@ require_once 'inc/collectd.inc.php';
 # yoctopuce-MODULENAME_SERIAL/gauge-FUNCTION.rrd
 
 $obj = new Type_Default($CONFIG);
-$obj->ds_names = array(
-        'value' => 'Value'
-);
 
-$obj->width  = $width;
-$obj->heigth = $heigth;
+//$obj->width  = $width;
+//$obj->heigth = $heigth;
 
 switch($obj->args['type']) {
-case 'humidity':
+case 'yoctopuce_humidity':
         $obj->rrd_title = sprintf('Humidity (%s)', $obj->args['pinstance']);
         $obj->rrd_vertical = '% Relative Humidity';
         $obj->rrd_format = '%5.1lf';
+        $obj->ds_names = array(
+                'yoctopuce_humidity'    => 'Humidity'
+        );
         break;
-case 'pressure':
+case 'yoctopuce_pressure':
         $obj->rrd_title = sprintf('Pressure (%s)', $obj->args['pinstance']);
         $obj->rrd_vertical = 'mBar';
         $obj->rrd_format = '%5.1lf';
+        $obj->ds_names = array(
+                'yoctopuce_pressure'    => 'Pressure'
+        );
         break;
-case 'temperature':
+case 'yoctopuce_temperature':
         $obj->rrd_title = sprintf('Temperature (%s)', $obj->args['pinstance']);
         $obj->rrd_vertical = 'Degrees Celsius';
         $obj->rrd_format = '%5.1lf%s';
+        $obj->ds_names = array(
+                'yoctopuce_temperature' => 'Temperature'
+        );
         break;
 }
 
