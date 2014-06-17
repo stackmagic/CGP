656bff488dfd45ba0ff95134e15ba467b45a213a
Patrick Huber <stackmagic@gmail.com>
2014-06-17 18:59:10 +0200

    add solr plugin based on tcpconns, stacked graph by origin field from facet query to solr

---
+++ plugin/solr.php
@@ -0,0 +1,18 @@
+<?php
+
+# Collectd layzapp solr plugin
+
+require_once 'conf/common.inc.php';
+require_once 'type/GenericStacked.class.php';
+require_once 'inc/collectd.inc.php';
+
+## LAYOUT
+# solr-CORE/solr_origin-ORIGIN.rrd
+
+$obj = new Type_GenericStacked($CONFIG);
+$obj->rrd_title = sprintf('Solr Content by Origin (%s)', $obj->args['pinstance']);
+$obj->rrd_vertical = '#';
+$obj->rrd_format = '%5.1lf';
+
+collectd_flush($obj->identifiers);
+$obj->rrd_graph();
